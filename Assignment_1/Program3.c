e31ee633c93b922475383fde480c6b27439cd22f
SakshiBajare <sakshibajare21@gmail.com>
2025-05-11 00:56:29 +0530

    Add files via upload

---
+++ Assignment_1/Program3.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+
+#include <stdio.h>
+
+void Display()
+ {
+    int i = 0;
+
+    i = 5;
+
+    while (i >= 1)
+     {
+        printf("%d ", i);
+        i--;
+    }
+}
+
+int main()
+ {
+    Display();
+    return 0;
+}
+
+
